9c5dce4af742b0da33861fac283edafe6a347d5a
Yaroslav <lunatik13@users.noreply.github.com>
2019-12-01 13:22:37 +0700

    Update spy.go

--- spy.go
+++ spy.go
@@ -39,6 +39,6 @@ type ConnIter interface {
 // If processes is true it'll additionally try to lookup the process owning the
 // connection, filling in the Proc field. You will need to run this as root to
 // find all processes.
-func Connections(processes bool) (ConnIter, error) {
-	return cbConnections(processes)
+func Connections(processes bool, tcpState) (ConnIter, error) {
+	return cbConnections(processes, tcpState)
 }
